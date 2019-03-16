e362776f4d0b12ec705aa1824dc93e7d058e91b5
Matsobane <joe_kekana@yahoo.com>
2019-03-16 18:59:21 +0200

    Committing the first project

---
+++ src/main/java/Jenkins/Test/Jenkins/App.java
@@ -0,0 +1,13 @@
+package Jenkins.Test.Jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
